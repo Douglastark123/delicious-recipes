341cad529c4f5d678c251fc439e95b84938e16a8
Douglas A. da Silva <59983373+Douglastark123@users.noreply.github.com>
2023-12-14 22:26:38 -0400

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'signup', component: SignupComponent},
+  { path: 'signin', component: SigninComponent},
+];
